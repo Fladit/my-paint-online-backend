075d139b4aed0aec64f92b14df2dd131a45d207d
Fladit <zatsarin77@gmail.com>
2021-01-04 02:29:35 +0300

    add registration, login and auth func

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -2,16 +2,20 @@ const {validationResult} = require("express-validator")
 const bcrypt = require("bcrypt")
 const User = require("../models/User")
 const Role = require("../models/Role")
+const jwt = require("jsonwebtoken")
 
 class AuthService {
-    //Добавить проверку на наличие уже зарегистрированного аккаунта с таким именем
-    async registratiion(req, res) {
+
+    async registration(req, res) {
         const errors = validationResult(req)
         if (!errors.isEmpty()) {
-            res.send(errors.array())
+            res.status(400).send(errors.array())
         }
         try {
             const username = req.body.username
+            const isUserExist = (await User.findOne({username: username})) !== null
+            if (isUserExist)
+                res.status(400).send({message: `User ${username} is already exist!`})
             const password = req.body.password
             const salt = bcrypt.genSaltSync()
             const hash = bcrypt.hashSync(password, salt)
@@ -29,21 +33,49 @@ class AuthService {
             })
             await user.save()
 
-            res.send({
-                username: user.username,
-                roles: user.roles,
-            })
+            const token = this.createJWT({username: user.username, roles: user.roles})
+            res.send({token})
         }
         catch (e) {
-            res.send(e.message)
+            res.status(400).send({message: e.message})
         }
 
     }
 
-    async authorization(req, res) {
+    async login(req, res) {
         const errors = validationResult(req)
         if (!errors.isEmpty()) {
             res.send(errors.array())
         }
+        const {username, password} = req.body
+        const user = await User.findOne({username})
+        if (!user)
+            res.status(400).send({message: `User ${username} is not found.`})
+        const isSuccessAuth = bcrypt.compareSync(password, user.password)
+        if (!isSuccessAuth)
+        {
+            res.status(400).send({message: "Incorrect password."})
+        }
+        const token = this.createJWT({username: user.username, roles: user.roles})
+        res.send({token})
     }
+
+    async authentication(req, res) {
+        const token = req.headers.authorization.split(" ")[1]
+        if (!token)
+            res.status(401).send("Authorization token is empty")
+        try {
+            const decodedUser = jwt.verify(token, process.env.PRIVATE_KEY)
+            res.status(200).send()
+        }
+        catch (e) {
+            res.status(400).send({message: e.message})
+        }
+    }
+
+    createJWT(user) {
+        const token = jwt.sign(user, process.env.PRIVATE_KEY)
+        return token
+    }
+
 }
